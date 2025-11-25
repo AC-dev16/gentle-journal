cf06cc0e70808b8e5bfaf6e1a2b6aab467bfffaf
AC-dev16 <amy_mb@hotmail.co.uk>
2025-11-25 10:34:15 +0000

    Configure database settings to use environment variable for DATABASE_URL

--- env.py
+++ env.py
@@ -2,3 +2,5 @@ import os
 
 os.environ.setdefault('SECRET_KEY', 'W)^,h>H];vr!e-{GYoO(oc,m&g};m$MkLy`ckUO.[$1tx?:Gy2d?skX=|OK(')
 
+os.environ.setdefault('DATABASE_URL', 'postgresql://neondb_owner:npg_Q2xLEKGhqDW7@ep-little-mode-agk7vb4k.c-2.eu-central-1.aws.neon.tech/lung_untie_young_43955')
+
